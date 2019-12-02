59c7acf149e4f81055858613f9b91b66472c813b
mxy-lf <2356569779@qq.com>
2019-12-02 18:03:24 +0800

    force -graph in onos web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { ResourceComponent } from './resource/resource.component';
 import { RoutingComponent } from './routing/routing.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { D3Service, D3_DIRECTIVES } from './d3';
+
+import { AppComponent } from './app.component';
+
+import { GraphComponent } from './visuals/graph/graph.component';
+import { SHARED_VISUALS } from './visuals/shared';
 @NgModule({
   declarations: [
     AppComponent,
     ResourceComponent,
-    RoutingComponent
+    RoutingComponent,
+    GraphComponent,
+    ...SHARED_VISUALS,
+    ...D3_DIRECTIVES
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [D3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
